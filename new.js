4a9e0cff75618d40e6db3b6b9a64ea61f1f68f70
SahilSingh8400 <sanju98099@gmail.com>
2026-01-30 12:45:30 +0530

    new files of node

---
+++ new.js
@@ -0,0 +1,16 @@
+// console.time("test");
+// for (let i=0;i<10000;i++);{}
+// console.timeEnd("test");
+
+// console.group('cse24');
+// console.log('name sahil');
+
+// console.groupEnd();
+
+// console.count('count');
+// console.count('count');
+// console.count('count');
+// console.count('count');
+// console.count('count');
+
+console.dir(document.body);
